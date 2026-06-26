9d5887c9ea6a922316205657c0d8eb39c0fb6cf7
Tanish Kashyap <hellobuffalo13@gmail.com>
2026-06-26 13:56:33 +0530

    Database starting

---
+++ Backend/app/core/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase,sessionmaker
+from app.core.config import settings
+
+Database_URL=(f"postgresql://{settings.DATABASE_USER}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOST}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}")
+engine=create_engine(Database_URL,echo=True)
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+class Base(DeclaritiveBase):
+    pass
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
