da8d99a311f6eda4fff37b751dc60822498fc8d9
Tanish Kashyap <hellobuffalo13@gmail.com>
2026-06-26 14:02:02 +0530

    Spelling Mistake fix

--- Backend/app/core/database.py
+++ Backend/app/core/database.py
@@ -5,7 +5,7 @@ from app.core.config import settings
 Database_URL=(f"postgresql://{settings.DATABASE_USER}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOST}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}")
 engine=create_engine(Database_URL,echo=True)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
-class Base(DeclaritiveBase):
+class Base(DeclarativeBase):
     pass
 def get_db():
     db=SessionLocal()
