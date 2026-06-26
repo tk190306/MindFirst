24e0489567792b4840432c12edeb59149ca58b76
Tanish Kashyap <hellobuffalo13@gmail.com>
2026-06-26 11:44:52 +0530

    Config coding

---
+++ Backend/app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+class Settings(BaseSettings):
+    DATABASE_HOST: str
+    DATABASE_PORT: int
+    DATABASE_NAME: str
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+settings = Settings()
