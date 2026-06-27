0137bb24017d9b005af0800d652c98fe45a88a6f
Tanish Kashyap <hellobuffalo13@gmail.com>
2026-06-27 15:18:58 +0530

    User table creation

--- Backend/app/models/user.py
+++ Backend/app/models/user.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from enum import Enum
 from uuid import UUID,uuid4
 from sqlalchemy import Boolean,DateTime,Enum as SQLEnum,String
@@ -8,4 +8,14 @@ from app.core.database import Base
 class UserRole(Enum):
     ADMIN = "admin"
     USER = "user"
+
+class User(Base):
+    __tablename__ = "users"
+    id:Mapped[UUID]=mapped_column(primary_key=True,default=uuid4)
+    email:Mapped[str]=mapped_column(String(255),unique=True,nullable=False, index=True)
+    password_hash:Mapped[str]=mapped_column(String(255),nullable=False)
+    role:Mapped[UserRole]=mapped_column(SQLEnum(UserRole),nullable=False,default=UserRole.USER)
+    is_active:Mapped[bool]=mapped_column(Boolean,default=True)
+    created_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),default=lambda: datetime.now(timezone.utc))
+    updated_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),default=lambda: datetime.now(timezone.utc),onupdate=lambda: datetime.now(timezone.utc))
     
