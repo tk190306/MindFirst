16b75d5404d8e3c2e071412196077c4756d2b3ae
Tanish Kashyap <hellobuffalo13@gmail.com>
2026-06-28 19:21:19 +0530

    Removed profiles table from user.py

--- Backend/app/models/user.py
+++ Backend/app/models/user.py
@@ -20,25 +20,3 @@ class User(Base):
     is_active:Mapped[bool]=mapped_column(Boolean,default=True)
     created_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),default=lambda: datetime.now(timezone.utc))
     updated_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),default=lambda: datetime.now(timezone.utc),onupdate=lambda: datetime.now(timezone.utc))
-
-class Gender(Enum):
-    MALE = "male"
-    FEMALE = "female"
-    NON_BINARY = "non-binary"
-    OTHER = "other"
-
-class Profile(Base):
-    __tablename__="profiles"
-    id:Mapped[UUID]=mapped_column(primary_key=True,default=uuid4)
-    user_id:Mapped[UUID]=mapped_column(ForeignKey("users.id"),unique=True,nullable=False)
-    first_name:Mapped[str]=mapped_column(String(225),nullable=False)
-    last_name:Mapped[str]=mapped_column(String(225),nullable=False)
-    birth_date:Mapped[date]=mapped_column(Date,nullable=False)
-    bio:Mapped[str]=mapped_column(String(255),nullable=True)
-    gender:Mapped[Gender]=mapped_column(SQLEnum(Gender),nullable=False)
-    attracted_to:Mapped[str]=mapped_column(String(255),nullable=True)
-    occupation:Mapped[str]=mapped_column(String(255),nullable=True)
-    city:Mapped[str]=mapped_column(String(255),nullable=True)
-    created_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),default=lambda: datetime.now(timezone.utc))
-    updated_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),default=lambda: datetime.now(timezone.utc),onupdate=lambda: datetime.now(timezone.utc))
-    pronouns:Mapped[str | None]=mapped_column(String(255),nullable=True)
