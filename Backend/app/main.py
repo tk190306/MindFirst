83bad892e1706ed14bea73cf74aaba7418dc1c6b
Tanish Kashyap <hellobuffalo13@gmail.com>
2026-06-29 14:08:09 +0530

    Authentication register added Happy

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.models import *
+from app.routers.auth import router as auth_router
 
 app=FastAPI()
+app.include_router(auth_router)
 
 @app.get("/")
 def root():
