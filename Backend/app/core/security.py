a397e3886a484c52e2a84713c745b5cf63d82755
Tanish Kashyap <hellobuffalo13@gmail.com>
2026-06-29 22:04:54 +0530

    added JWT token verification

--- Backend/app/core/security.py
+++ Backend/app/core/security.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta, timezone
-from jose import jwt
+from fastapi import HTTPException
+from jose import jwt,JWTError
 from passlib.context import CryptContext
 from app.core.config import settings
 from uuid import UUID
@@ -16,3 +17,14 @@ def create_access_token(user_id:UUID)->str:
     expire= datetime.now(timezone.utc)+timedelta(days=30)
     to_encode={"exp": expire, "sub": str(user_id)}
     return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+def verify_access_token(token:str)->UUID:
+    try:
+        payload=jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        user_id=payload.get("sub")
+        if user_id is None:
+            raise ValueError("Invalid token")
+        return UUID(user_id)
+    except JWTError as e:
+        raise HTTPException(status_code=401, detail="Invalid token") from e
+    
