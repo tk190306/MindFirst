5d2563ea6d9ad34a47092d307e3022e8677c6c60
Tanish Kashyap <hellobuffalo13@gmail.com>
2026-06-29 14:33:57 +0530

    User repository created

---
+++ Backend/app/repositories/user_repositories.py
@@ -0,0 +1,18 @@
+from uuid import UUID
+from sqlalchemy.orm import Session
+from app.models.user import User
+
+class UserRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_user_by_email(self, email: str) -> User|None:
+        return self.db.query(User).filter(User.email == email).first()
+
+    def create_user(self, user: User) -> User:
+        self.db.add(user)
+        self.db.commit()
+        self.db.refresh(user)
+        return user
+    def get_user_by_id(self, user_id: UUID) -> User|None:
+        return self.db.query(User).filter(User.id == user_id).first()
