48782de7d8657b0a7cde6b1bf609e0bae42a8f39
Tanish Kashyap <hellobuffalo13@gmail.com>
2026-06-30 17:18:03 +0530

    Added dependeicies for Authentication

--- Backend/app/dependencies/auth.py
+++ Backend/app/dependencies/auth.py
@@ -1,4 +1,18 @@
+from fastapi import Depends,HTTPException,status
 from fastapi.security import OAuth2PasswordBearer
-
+from app.core.security import verify_access_token
+from app.repositories.user_repositories import UserRepository
+from sqlalchemy.orm import Session
+from app.core.database import get_db
+from app.models.user import User
 oauth2_scheme=OAuth2PasswordBearer(tokenUrl="/auth/login")
-
+def get_current_user(token:str=Depends(oauth2_scheme),db:Session=Depends(get_db))->User:
+    user_id=verify_access_token(token)
+    user_repository=UserRepository(db)
+    user=user_repository.get_user_by_id(user_id)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials"
+        )
+    return user
